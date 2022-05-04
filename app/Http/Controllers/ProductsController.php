5695e88a8c95f171946ffeb9d0d0883a08dcf797
Pedro <pedrobastos750@gmail.com>
2022-05-03 23:21:25 -0400

    CRUD CREATE

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'slug' => 'required',
+            'price' => 'required'
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $request->validate([
+            'nome' => 'required',
+            'slug' => 'required',
+            'price' => 'required'
+        ]);
+
+        $product->nome = $request->nome;
+        $product->slug = $request->slug;
+        $product->price = $request->price;
+        $product->save();
+
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Product::destroy($id);
+    }
+
+
+    public function search($nome)
+    {
+        return Product::where('nome', 'like', '%'.$name.'%')->get();
+    }
+}
